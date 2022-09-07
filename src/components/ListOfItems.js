d74f925ae908c5120b01b5da0af57399456b870e
Andriy Konzelskiy <pro100andrik94@gmail.com>
2022-09-07 11:32:58 +0300

    removed close button visibility because now it's in CSS

--- src/components/ListOfItems.js
+++ src/components/ListOfItems.js
@@ -10,14 +10,6 @@ const ListOfItems = props =>{
     props.handleDeleteItem(index)
   }
 
-  const handleMouseEnter = index => {
-    props.handleButtonVisibility(index);
-  }
-
-  const handleMouseLeave = index => {
-    props.handleButtonVisibility(index);
-  }
-
 
     let filteredTasks = props.taskList;
     if(props.showType === 'showActive'){
@@ -27,18 +19,16 @@ const ListOfItems = props =>{
     }
     return(
       filteredTasks.map((element, index) =>
-                          <li key={index}
-                          onMouseEnter={() => handleMouseEnter(index)}
-                          onMouseLeave={() => handleMouseLeave(index)}>
+                          <li key={index}>
                             <input type='checkbox'
                               value={element.index}
                               checked={element.isChecked}
                               onChange={handleCheck}
                               className='checkbox'>
                             </input>
-                            
+
                             <span className={element.isChecked ? 'text checked-element' : 'text'} >{element.task}</span>
-                            <button className={props.buttonIsVisible[index] ? 'close-button' : 'close-button hiden'}
+                            <button className='close-button'
                             onClick={() => handleButtonClick(index)}>X</button>
                           </li>)
     )
